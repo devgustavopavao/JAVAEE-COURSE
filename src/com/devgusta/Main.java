9bd872c789b64a44c0577073bddb7d4af401a7b0
DevGusta <gusta.pavao@gmail.com>
2023-11-08 01:16:41 -0400

    Criação da class Pessoa

--- src/com/devgusta/Main.java
+++ src/com/devgusta/Main.java
@@ -1,4 +1,6 @@
- public class Main {
+package com.devgusta;
+
+public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello and welcome this a git test in INTELLIJ IDEA !");
